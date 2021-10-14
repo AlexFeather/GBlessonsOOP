2765bccff36419e03cddacd36849363c19898b39
AlexFeather <alfeat@gmail.com>
2021-10-14 17:08:16 +0300

    added solution

---
+++ OopLesson/Account.cs
@@ -0,0 +1,60 @@
+﻿using static OopLesson.Program;
+
+namespace OopLesson
+{
+    public class Account
+    {
+        private static int idCounter = 0;
+
+        public int Total { get; private set; }
+        public AccountType Type { get; private set; }
+
+        public int Id { get; private set; }
+
+
+        public Account(AccountType type)
+        {
+            GenerateId();
+            Type = type;
+            Total = 0;
+        }
+
+        public Account(int initialSum)
+        {
+            GenerateId();
+            Total = initialSum;
+        }
+
+        public Account(AccountType type, int initialSum)
+        {
+            GenerateId();
+            Type = type;
+            Total = initialSum;
+        }
+
+
+        public int GenerateId()
+        {
+            idCounter++;
+            return idCounter;
+        }
+
+        public void DepositIntoAccount(int sum)
+        {
+            Total += sum;
+        }
+
+        public bool WithdrawFromAccount(int sum)
+        {
+            if (sum <= Total)
+            {
+                Total -= sum;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
