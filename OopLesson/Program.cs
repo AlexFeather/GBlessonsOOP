09be81ddab45c7764d9e6708cc62266118a52249
AlexFeather <alfeat@gmail.com>
2021-10-21 15:25:53 +0300

    Added StringReverse Method

--- OopLesson/Program.cs
+++ OopLesson/Program.cs
@@ -11,7 +11,7 @@ namespace OopLesson
         }
         static void Main(string[] args)
         {
-            
+
         }
 
     }
---
+++ OopLesson/StringsWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLesson
+{
+    class StringsWork
+    {
+
+        public string StringReverse(string input)
+        {
+            StringBuilder builder = new StringBuilder();
+            for(int i = 0; i<input.Length; i++)
+            {
+                builder.Insert(0, input[i]);
+            }
+            return builder.ToString();
+        }
+
+    }
+}
